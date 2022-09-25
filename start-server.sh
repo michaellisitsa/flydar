2f2cccc54e7345f8edff70442e9a48a3a2f0ec9b
dmc <d.massey.cole@gmail.com>
2022-09-25 16:28:38 +1000

    Updated start-server script to manage group permissions creation

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,6 @@
 python manage.py collectstatic --noinput;  # create static pages
 python manage.py makemigrations;  # create initial db migrations
 python manage.py migrate;  # make the db
+python manage.py create_groups;  # create default groups and permissons
 
 python manage.py runserver 0.0.0.0:8000;  # run the test server
