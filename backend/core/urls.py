37ca63ef757554aa4dbc5db45191994135ad1138
dmc <d.massey.cole@gmail.com>
2022-09-26 21:57:07 +1000

    Added dyanamic link to pest trap record on pest trap table

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("pest-trap-registration/", views.pest_trap_registration, name="pest-trap-registration"),
     path("pest-trap-table/", views.pest_trap_table, name="pest-trap-table"),
-    path("pest-trap/<int:id>/", views.pest_trap_record),
+    path("pest-trap/<int:id>/", views.pest_trap_record, name="pest-trap"),
     path("observation-registration/", views.observation_registration, name="observation-registration"),
     path("observation-table/", views.observation_table, name="observation-table")
 ]
